86cb2bae4e641630eb70d2d9f613150276d4fe6f
Darius <102800951+DariusCristian@users.noreply.github.com>
2024-05-29 14:45:23 +0300

    Create Laborator12.c

---
+++ Laborator12.c
@@ -0,0 +1,6 @@
+// Problema de laborator
+/* Se citeşte un număr întreg n după care se citesc n propoziţii 
+(fiecare terminată cu tasta enter). Să se
+sorteze aceste propoziţii (sortare de şiruri de caractere). */
+
+
